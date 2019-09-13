ae6bb93dda214258ba0e5e0a17c3fbc2639d1a00
Jaime Tyler <jaimetyler88@gmail.com>
2019-09-12 23:45:25 -0400

    deployed to firebase, going to update readme and organize project before continuing

---
+++ assets/javascript/app.js
@@ -0,0 +1,7 @@
+console.log('Yo im here!');
+
+$('.jumbotron').css({ height: $(window).height() + 'px' });
+
+$(window).on('resize', function() {
+  $('.jumbotron').css({ height: $(window).height() + 'px' });
+});
