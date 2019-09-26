8cf8e7e76093c6e206f7e20518f96b85f6c2f796
Jaime Tyler <jaimetyler88@gmail.com>
2019-09-26 16:48:22 -0400

    testing video background instead of image for homepage. created other pages and linked to nav. Need to polish up components to recycle on other pages with differnt displays.

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -5,3 +5,17 @@ $('.jumbotron').css({ height: $(window).height() + 'px' });
 $(window).on('resize', function() {
   $('.jumbotron').css({ height: $(window).height() + 'px' });
 });
+
+//****/javascript for background video****//
+// function deferVideo() {
+//   //defer html5 video loading
+//   $('video source').each(function() {
+//     var sourceFile = $(this).attr('data-src');
+//     $(this).attr('src', sourceFile);
+//     var video = this.parentElement;
+//     video.load();
+//     // uncomment if video is not autoplay
+//     video.play();
+//   });
+// }
+// window.onload = deferVideo;
