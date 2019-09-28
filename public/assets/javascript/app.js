898ddf1a128c498d35c7e4642816c733d0ca7fc7
Jaime Tyler <jaimetyler88@gmail.com>
2019-09-28 16:09:07 -0400

    cleanup

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -6,16 +6,11 @@ $(window).on('resize', function() {
   $('.jumbotron').css({ height: $(window).height() + 'px' });
 });
 
-//****/javascript for background video****//
-// function deferVideo() {
-//   //defer html5 video loading
-//   $('video source').each(function() {
-//     var sourceFile = $(this).attr('data-src');
-//     $(this).attr('src', sourceFile);
-//     var video = this.parentElement;
-//     video.load();
-//     // uncomment if video is not autoplay
-//     video.play();
-//   });
-// }
-// window.onload = deferVideo;
+// ********** FIREBASE ********** //
+
+SpaceRace.prototype.addShip = function(data) {
+  const collection = firebase.firestore().collection('inquery');
+  return collection.add(data);
+};
+
+// ********** END FIREBASE ********** //
