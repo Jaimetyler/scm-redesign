fcdf436335a841c91abb8d4b53ca992927161f9d
Jaime Tyler <jaimetyler88@gmail.com>
2019-10-06 11:04:20 -0400

    commented in index.html and warehouse.html and broke down where bootstrap is being used. Tried explaining what everything is doing. Also commented app.js page where Jquery is being used. All links and resources being loaded are necessary for the current page.

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,6 +1,7 @@
 console.log('Yo its the app!');
 
 // ********** Navbar Scroll  ********** //
+// JQuery being used here
 $(document).ready(function() {
   $(window).scroll(function() {
     // check if scroll event happened
@@ -14,47 +15,47 @@ $(document).ready(function() {
 });
 
 // ********** Jumbotron Image as Background ********** //
+// JQuery being used here
 $('.jumbotron').css({ height: $(window).height() + 'px' });
 
 $(window).on('resize', function() {
   $('.jumbotron').css({ height: $(window).height() + 'px' });
 });
 
-// ********** Firebase login********** //
-
 // ********** Firebase form save********** //
+//*********** */This was me starting to try to work on the Form. Feel free to delete*******//
 
-const firstName = document.getElementById('firstName');
-const lastName = document.getElementById('lastname');
-const email = document.getElementById('emailAddress');
-const company = document.getElementById('company');
-const serviceType = document.getElementById('serviceType');
-const comment = document.getElementById('comment');
+// const firstName = document.getElementById('firstName');
+// const lastName = document.getElementById('lastname');
+// const email = document.getElementById('emailAddress');
+// const company = document.getElementById('company');
+// const serviceType = document.getElementById('serviceType');
+// const comment = document.getElementById('comment');
 
-function onMessageFormSubmit(e) {
-  e.preventDefault();
-  // Check that the user entered a message and is signed in.
-  if (email.value && checkSignedInWithMessage()) {
-    saveInquiry(Inquiry.value).then(function() {
-      // Clear message text field and re-enable the SEND button.
-      resetMaterialTextfield(messageInputElement);
-      toggleButton();
-    });
-  }
-}
+// function onMessageFormSubmit(e) {
+//   e.preventDefault();
+//   // Check that the user entered a message and is signed in.
+//   if (email.value && checkSignedInWithMessage()) {
+//     saveInquiry(Inquiry.value).then(function() {
+//       // Clear message text field and re-enable the SEND button.
+//       resetMaterialTextfield(messageInputElement);
+//       toggleButton();
+//     });
+//   }
+// }
 
-function saveMessage(messageText) {
-  // Add a new message entry to the Firebase database.
-  return firebase
-    .firestore()
-    .collection('messages')
-    .add({
-      name: getUserName(),
-      text: messageText,
-      profilePicUrl: getProfilePicUrl(),
-      timestamp: firebase.firestore.FieldValue.serverTimestamp()
-    })
-    .catch(function(error) {
-      console.error('Error writing new message to Firebase Database', error);
-    });
-}
+// function saveMessage(messageText) {
+//   // Add a new message entry to the Firebase database.
+//   return firebase
+//     .firestore()
+//     .collection('messages')
+//     .add({
+//       name: getUserName(),
+//       text: messageText,
+//       profilePicUrl: getProfilePicUrl(),
+//       timestamp: firebase.firestore.FieldValue.serverTimestamp()
+//     })
+//     .catch(function(error) {
+//       console.error('Error writing new message to Firebase Database', error);
+//     });
+// }
